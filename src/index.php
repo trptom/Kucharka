200c20f0ddae30c21d54c67646826694614a1c8c
trptom <trptom@147.32.82.47>
2012-09-26 13:27:06 +0200

    Uprava struktury repozitare, nova vize.

--- src/index.php
+++ src/index.php
@@ -3,10 +3,13 @@
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
         <title></title>
+        <!-- Include the bootstrap stylesheets -->
+        <link rel="stylesheet" href="">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <div class="container">
+            
+        </div>
     </body>
 </html>
+    
