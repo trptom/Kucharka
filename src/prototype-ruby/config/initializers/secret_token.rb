91f1271a1f285fe368f03377b4b0bae21306033e
Administrator <prasltom@fel.cvut.cz>
2012-10-07 22:44:42 +0200

    Složka a základní vzor projektu, psaného v Ruby on Rails.

---
+++ src/prototype-ruby/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrototypeRuby::Application.config.secret_token = '2ec356f70042e8eb4f2d113a972091df5be8ec0d1eeeda4889ce8add70110b4bc10c0254746fb90d30f952d9fe21e5fde8d0bf519710cea29c2000f8461aee24'
