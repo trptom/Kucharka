74ca9298a18617f3270f891643ca509449f85ccb
Administrator <prasltom@fel.cvut.cz>
2012-11-05 20:18:11 +0100

    Smazani stareho rails projektu.

--- src/rails/Kucharka/app/models/user.rb
+++
@@ -1,3 +0,0 @@
-class User < ActiveRecord::Base
-  attr_accessible :description, :email, :id, :login, :password, :userName
-end
--- src/rails/Kucharka/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Kucharka::Application.config.secret_token = '6072ce249047d5db170c7a22227cfc3c33d71c49afe0c60f9f2418ff459c6324024defc4c8e830842c75d04c1c75b0fb97bae3371c2b574ae6d9ac63230fc9ea'
