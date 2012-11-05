5f65a582a26157c9a8dbd180075c6517c0f2e928
Administrator <prasltom@fel.cvut.cz>
2012-11-05 20:11:13 +0100

    Prihlasovani a odhlasovani v aplikaci.

---
+++ src/rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PokusSorcery::Application.config.secret_token = '64f9682dc0522ef7a39240f3eccf6920b03f8128eaf072a13d4c5b713b4778d5535dd061b655ca90850e49870f19ba654ea6563d33630fc244e0f78f9eae0f2b'
