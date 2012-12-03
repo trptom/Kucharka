268c9d39db77ece7ce4a75e7ccdc037c775bdb14
Szép Gergely <szepgerg@fel.cvut.cz>
2012-12-03 23:38:14 +0100

    pridany javascripty
    lednicka, hodnoceni, novy recept

--- oth/graficke navrhy/web/bootstrap/js/lednicka.js
+++ oth/graficke navrhy/web/bootstrap/js/lednicka.js
@@ -3,11 +3,89 @@
  * and open the template in the editor.
  */
 
-function addToFridge(){
-    var ledDiv = document.getElementById("lednicka");
-    var tmp = document.createElement("span");
-    tmp.setAttribute("class", "label");
-    var text = document.createTextNode("ingredience");
-    tmp.appendChild(text);
-    ledDiv.appendChild(tmp);
-}
+$(document).ready(function(){
+    $('#addButton').click(function(){
+        var text = $('#inputIngr').val();
+        if(text != ""){
+        $('#ingredience').append('<a class="x"><span class ="label">'+ text +'</span><i class="icon-remove"></a>');
+        $('#inputIngr').val("");
+        }
+    });
+});
+
+$(document).ready(function(){
+    $('.x').live('click', function(){
+        $(this).remove();
+    }); 
+});
+
+$(document).ready(function(){
+    $('#star1').mouseover(function(){
+        $('#star1').attr({class: "btn btn-small btn-warning"});
+    });
+    $('#star1').mouseout(function(){
+        $('#star1').attr({class: "btn btn-small"});
+    });
+    $('#star2').mouseover(function(){
+        $('#star1').attr({class: "btn btn-small btn-warning"});
+        $('#star2').attr({class: "btn btn-small btn-warning"});
+    });
+    $('#star2').mouseout(function(){
+        $('#star1').attr({class: "btn btn-small"});
+        $('#star2').attr({class: "btn btn-small"});
+    });
+    $('#star3').mouseover(function(){
+        $('#star1').attr({class: "btn btn-small btn-warning"});
+        $('#star2').attr({class: "btn btn-small btn-warning"});
+        $('#star3').attr({class: "btn btn-small btn-warning"});
+    });
+    $('#star3').mouseout(function(){
+        $('#star1').attr({class: "btn btn-small"});
+        $('#star2').attr({class: "btn btn-small"});
+        $('#star3').attr({class: "btn btn-small"});
+    });
+    $('#star4').mouseover(function(){
+        $('#star1').attr({class: "btn btn-small btn-warning"});
+        $('#star2').attr({class: "btn btn-small btn-warning"});
+        $('#star3').attr({class: "btn btn-small btn-warning"});
+        $('#star4').attr({class: "btn btn-small btn-warning"});
+    });
+    $('#star4').mouseout(function(){
+        $('#star1').attr({class: "btn btn-small"});
+        $('#star2').attr({class: "btn btn-small"});
+        $('#star3').attr({class: "btn btn-small"});
+        $('#star4').attr({class: "btn btn-small"});
+    });
+    $('#star5').mouseover(function(){
+        $('#star1').attr({class: "btn btn-small btn-warning"});
+        $('#star2').attr({class: "btn btn-small btn-warning"});
+        $('#star3').attr({class: "btn btn-small btn-warning"});
+        $('#star4').attr({class: "btn btn-small btn-warning"});
+        $('#star5').attr({class: "btn btn-small btn-warning"});
+    });
+    $('#star5').mouseout(function(){
+        $('#star1').attr({class: "btn btn-small"});
+        $('#star2').attr({class: "btn btn-small"});
+        $('#star3').attr({class: "btn btn-small"});
+        $('#star4').attr({class: "btn btn-small"});
+        $('#star5').attr({class: "btn btn-small"});
+    });
+    
+    $('#star1').click(function(){
+       window.alert("Vaše hodnocení: 1"); 
+    });
+    $('#star2').click(function(){
+       window.alert("Vaše hodnocení: 2"); 
+    });
+    $('#star3').click(function(){
+       window.alert("Vaše hodnocení: 3"); 
+    });
+    $('#star4').click(function(){
+       window.alert("Vaše hodnocení: 4"); 
+    });
+    $('#star5').click(function(){
+       window.alert("Vaše hodnocení: 5"); 
+    });
+});
+
+
