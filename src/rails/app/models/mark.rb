dd81d387873d0cad653a6eeab0b153f0c2029967
trptom <prasltom@fel.cvut.cz>
2012-12-11 21:38:42 +0100

    Úprava m:n vztahů
    ... pro nově vytvořené tabulky. Všechno by teď mělo sedět podle návrhu.

--- src/rails/app/models/mark.rb
+++ src/rails/app/models/mark.rb
@@ -1,5 +1,6 @@
 class Mark < ActiveRecord::Base
   belongs_to :recipe
   belongs_to :user
+  
   attr_accessible :note, :value
 end
--- src/rails/app/models/recipe.rb
+++ src/rails/app/models/recipe.rb
@@ -3,6 +3,8 @@ class Recipe < ActiveRecord::Base
 
   has_many :comments
   has_many :recipeCategoryLinks
+  has_many :recipeArticleLinks
+  has_many :recipeIngredienceLinks
 
   attr_accessible :annotation, :content, :name, :level, :estimated_time, :created_at
 end
