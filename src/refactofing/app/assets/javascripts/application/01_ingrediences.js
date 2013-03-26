c90e4b4c1a87b6c8e9e1e641b6ef47a7405b36b3
Tomas Praslicak <tpraslicak@seznam.cz>
2013-03-26 20:39:29 +0100

    Rozšíření image uploaderu, lightbox ingrediencí
    Uploader už nahrává i zmenšené obrázky, lightbox je ve fázi návrhu.
    Přidal jsem knihovnu bootstrap-lightbox.

---
+++ src/refactofing/app/assets/javascripts/application/01_ingrediences.js
@@ -0,0 +1,5 @@
+$(document).ready(function() {
+    $('#demoLightbox').lightbox({
+        show: false
+    });
+});
--- src/refactofing/app/uploaders/recipe_image_uploader.rb
+++ src/refactofing/app/uploaders/recipe_image_uploader.rb
@@ -4,7 +4,7 @@ class RecipeImageUploader < CarrierWave::Uploader::Base
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
-  # include CarrierWave::MiniMagick
+ include CarrierWave::MiniMagick
 
   # Include the Sprockets helpers for Rails 3.1+ asset pipeline compatibility:
   # include Sprockets::Helpers::RailsHelper
@@ -35,16 +35,17 @@ class RecipeImageUploader < CarrierWave::Uploader::Base
   #   # do something
   # end
 
-  # Create different versions of your uploaded files:
-  # version :thumb do
-  #   process :scale => [50, 50]
-  # end
+  process :resize_to_fit => [600, 600]
+
+  version :thumb do
+    process :resize_to_fit => [50,50]
+  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_white_list
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_white_list
+    %w(jpg jpeg gif png)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
