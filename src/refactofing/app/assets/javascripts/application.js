ebf0e91ede7ddbad59ea4b4adf83b4a16f2526f8
Tomas Praslicak <tpraslicak@seznam.cz>
2013-04-11 13:06:42 +0200

    Oprava mazani, dodelani user_sessions
    Pridavam zapomenuty soubor s user_sessions.
    Oprava mazani (uz by melo vsude fungovat - je potreba jquery_uis v
    application.js)

--- src/refactofing/app/assets/javascripts/application.js
+++ src/refactofing/app/assets/javascripts/application.js
@@ -11,5 +11,6 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 //= require jquery
+//= require jquery_ujs
 //= require_tree ./bootstrap
 //= require_tree ./application
--- src/refactofing/app/controllers/user_sessions_controller.rb
+++ src/refactofing/app/controllers/user_sessions_controller.rb
@@ -3,11 +3,15 @@
 class UserSessionsController < ApplicationController
   before_filter :require_login, :only => [:destroy]
 
+  def new
+    @user = User.new
+  end
+
   def create
     respond_to do |format|
       if params[:username] && params[:password] &&
           @user = login(params[:username],params[:password])
-        format.html { redirect_back_or_to("/", :notice => 'Pčihlašování proběhlo úspěšně.') }
+        format.html { redirect_to("/", :notice => 'Prihlašování proběhlo úspěšně.') }
         format.xml { render :xml => @user, :status => :created, :location => @user }
       else
         format.html { redirect_to( "/home/error", :notice => "Přihlašování selhalo.") }
@@ -18,6 +22,6 @@ class UserSessionsController < ApplicationController
 
   def destroy
     logout
-    redirect_to( "/", :notice => 'Logged out!')
+    redirect_to( "/", :notice => 'Uživatel byl úspěšne odhlášen!')
   end
 end
