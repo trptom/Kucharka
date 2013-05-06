6f6c23bcad42e512eb69e16bc4b7e5ae27be194c
Tomas Praslicak <tpraslicak@seznam.cz>
2013-05-06 15:01:07 +0200

    Code coverage
    Integrovani simplecov, archiv prvotnich vysledku z 5.5.

--- src/refactofing/app/controllers/home_controller.rb
+++ src/refactofing/app/controllers/home_controller.rb
@@ -6,8 +6,8 @@ class HomeController < ApplicationController
 
   def index
     @newest = get_recipes_sorted_by_date(5)
-    @random = get_recipes_by_random(5)
-    @best = get_recipes_by_mark(5)
+    @random = get_recipes_sorted_by_random(5)
+    @best = get_recipes_sorted_by_mark(5)
   end
 
   def search
--- src/refactofing/test/test_helper.rb
+++ src/refactofing/test/test_helper.rb
@@ -1,5 +1,27 @@
 # coding:utf-8
 
+require 'simplecov'
+SimpleCov.start do
+  # odfiltruju testy, aby mi to nekazilo celkovy vysledky
+  add_filter "test/"
+  # taky vyhodim config - vzdy se projedou cele
+  add_filter "config/"
+  
+  # zakladni skupiny podle typu souboru
+  add_group "Models", "app/models"
+  add_group "Controllers", "app/controllers"
+  add_group "Mailers", "app/mailers"
+  add_group "Uploaders", "app/uploaders"
+  add_group "Helpers", "app/helpers"
+  # a jeste neco navic
+  add_group "Long files (200+)" do |src_file|
+    src_file.lines.count > 200
+  end
+  add_group "Short files (10-)" do |src_file|
+    src_file.lines.count <= 10
+  end
+end
+
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
